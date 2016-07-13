84a2163a9ce1de5d1afd3d60102ef87791c05d01
Yuri Calabrez <yuri.calabrez@gmail.com>
2016-07-12 22:56:25 -0300

    Primeira aplicação

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -8,6 +8,7 @@ return [
         ],
         'factories' => [
             App\Action\HomePageAction::class => App\Action\HomePageFactory::class,
+            App\Action\TestePageAction::class => App\Action\TestePageFactory::class,
         ],
     ],
 
@@ -22,6 +23,12 @@ return [
             'name' => 'api.ping',
             'path' => '/api/ping',
             'middleware' => App\Action\PingAction::class,
+            'allowed_methods' => ['GET'],
+        ],
+        [
+            'name' => 'teste',
+            'path' => '/teste',
+            'middleware' => App\Action\TestePageAction::class,
             'allowed_methods' => ['GET'],
         ],
     ],
---
+++ src/App/Action/TestePageAction.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Action;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Expressive\Router;
+use Zend\Expressive\Template;
+use Zend\Expressive\Plates\PlatesRenderer;
+use Zend\Expressive\Twig\TwigRenderer;
+use Zend\Expressive\ZendView\ZendViewRenderer;
+
+class TestePageAction
+{
+
+    private $template;
+
+    public function __construct(Template\TemplateRendererInterface $template = null)
+    {
+        $this->template = $template;
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
+    {
+        return new HtmlResponse($this->template->render("app::teste", ['data' => "Minha primeira aplicação"]));
+    }
+}
